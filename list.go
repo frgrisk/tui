bd142082788a007d2f0b3581cefec89926ef1817
Henry Skiba <henryskiba@gmail.com>
2025-03-17 08:17:41 +0800

    Export `DefaultStyle` with adaptive color scheme (#21)

--- list.go
+++ list.go
@@ -20,7 +20,7 @@ const defaultWidth = 80
 var (
 	titleStyle        = lipgloss.NewStyle().MarginLeft(2)
 	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
-	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color(frgLime))
+	selectedItemStyle = DefaultStyle.PaddingLeft(2)
 	paginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
 	helpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
 	quitTextStyle     = lipgloss.NewStyle().Margin(1, 0, 2, 4)
